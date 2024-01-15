53ef8a88b17a80eb2e606c3bd19584ec4a52d15f
pagordm <pablo.gordillo@alum.unirioja.es>
2024-01-15 14:30:14 +0100

    Update to 1.20.4

--- src/main/java/com/pablogormi/magicfoods/mixin/PlayerEntityMixin.java
+++ src/main/java/com/pablogormi/magicfoods/mixin/PlayerEntityMixin.java
@@ -1,13 +1,17 @@
 package com.pablogormi.magicfoods.mixin;
 
 import com.pablogormi.magicfoods.entity.effect.ModStatusEffects;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,13 +19,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
 
+    @Final
+    @Shadow
+    private PlayerAbilities abilities;
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    @Inject(at = @At("RETURN"), method = "eatFood")
-    protected void onEatFood(World world, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
-        this.addStatusEffect(new StatusEffectInstance(ModStatusEffects.FLYING_STATUS_EFFECT, 5*20));
+    @Inject(at = @At("RETURN"), method = "getAbilities")
+    protected void onGetAbilities(CallbackInfoReturnable<PlayerAbilities> cir) {
+        if (this.getWorld().isClient()) {
+            this.abilities.allowFlying = this.hasStatusEffect(ModStatusEffects.FLYING_STATUS_EFFECT) || this.abilities.creativeMode;
+            if (!this.abilities.creativeMode && !this.hasStatusEffect(ModStatusEffects.FLYING_STATUS_EFFECT))
+                this.abilities.flying = false;
+        }
     }
 
 
